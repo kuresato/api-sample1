71269e2a255b4e40bcc826d31374f03f6de93403
kuresato <kuresato@gmail.com>
2020-06-17 18:26:59 +0900

    add booksBookIdGet

--- src/main/java/apis/handler/BooksApiDelegateImpl.java
+++ src/main/java/apis/handler/BooksApiDelegateImpl.java
@@ -17,15 +17,24 @@ public class BooksApiDelegateImpl implements BooksApiDelegate {
     @Override
     public ResponseEntity<List<Book>> booksGet() {
         List<Book> books = new ArrayList<Book>();
-        Book book = new Book();
-        book.id("id1");
-        book.title("title1");
-        book.author("author1");
-        book.author2("author1-2");
-        books.add(book);
+        books.add(createBook("1", "title1", "author1", "author2"));
+        books.add(createBook("2", "title2", "author1", "author2"));
 
         //return ResponseEntity.ok().body(books);
         return ResponseEntity.ok(books);
     }
-    
+
+    @Override
+    public ResponseEntity<Book> booksBookIdGet(String bookId) {
+        return ResponseEntity.ok(createBook(bookId, "title"+bookId, "author1", "author2"));
+    }
+
+    private Book createBook(String id, String title, String author1, String author2) {
+        Book book = new Book();
+        book.id(id);
+        book.title(title);
+        book.author(author1);
+        book.author2(author2);
+        return book;
+    }
 }
